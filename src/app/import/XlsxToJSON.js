c7acdaac446bebe9e38c6d6c87aadf924fbbdf09
ShantiLKael <98842719+ShantiLKael@users.noreply.github.com>
2024-03-27 11:11:44 +0100

    XLSXToJSON

--- src/app/import/XlsxToJSON.js
+++ src/app/import/XlsxToJSON.js
@@ -2,6 +2,7 @@
 const { insertEtudiant, insertCompetence, insertModule, insertMoyenne } = require('DB.js');
 const XLSX = require('xlsx');
 
+/* TEST
 const spreadSheet = XLSX.readFile('test.xlsx');
 let colums = {};
 for (const columName of spreadSheet.SheetNames) 
@@ -9,24 +10,76 @@ for (const columName of spreadSheet.SheetNames)
 	colums[columName] = XLSX.utils.sheet_to_json(spreadSheet.Sheets[columName]);
 }
 
-console.log(JSON.stringify(colums.Sheet1), "\n\n");
+console.log(JSON.stringify(colums.Sheet1), "\n\n");*/
 
 let fileMoy = document.getElementById('fichier_import');
-fileMoy.addEventListener('change', () =>
+fileMoy.addEventListener('change', (event) =>
 {
-	const file = fileMoy.files[0];
+	const file = event.target.files[0]; 
 	const reader = new FileReader();
 
-	reader.onload = (event) => 
+	reader.onload = function (event) 
 	{
-		const data = event.target.result;
+        const data = new Uint8Array(event.target.result);
 		const workbook = XLSX.read(data, {type:'array'});
 		const sheet = workbook.SheetNames[0];
 		const worksheet = workbook.Sheets[sheet];
 		const rows = XLSX.utils.sheet_to_json(worksheet);
+		const bonus = null;
+
+		// We get all the informations about competences
+		const compDetails = rows[0].slice(13, rows[0].length-4);
+		for (let i=0; i < compDetails.length; i++)
+		{
+			let key = compDetails[i];
+
+			if (key.startsWith('Bonus') && is_null(bonus))
+			{
+				bonus = row[key];
+			}
+
+			if (key.startsWith('BIN'))
+			{
+				const comp  = compDetails[i];
+				let lib   = comp;
+				let id    = comp.replace('BIN','');
+				let semId = comp.replace('BIN','').charAt(0);
+
+				let competence = 
+				{
+					id   : id,
+					lib  : lib,
+					semId: semId,
+				};
+			
+				insertCompetence(competence)
+				.then(insertedData => {
+					console.log('Inserted data:', insertedData);
+				})
+				.catch(error => {
+					console.error('Error:', error);
+				});
+
+				let module = 
+				{
+					//TODO 
+					id : ,
+					lib: ,
+				};
+				
+				insertModule(module)
+				.then(insertedData => {
+					console.log('Inserted data:', insertedData);
+				})
+				.catch(error => {
+					console.error('Error:', error);
+				});
+			}
+		}
 
 		for (let row of rows)
 		{
+
 			let etudiant = 
 			{
 				id : row['etudid'],
@@ -35,7 +88,7 @@ fileMoy.addEventListener('change', () =>
 				td : row['TD'],
 				tp : row['TP'],
 				bac: row['Bac'],
-				bonus : row[15],
+				bonus : row[bonus],
 				prenom: row['Prénom'],
 				parcours: row['Cursus'],
 			};
@@ -48,35 +101,11 @@ fileMoy.addEventListener('change', () =>
 				console.error('Error:', error);
 			});
 
-			let module = 
-			{
-				//TODO 
-			};
-			
-			insertModule(module)
-			.then(insertedData => {
-				console.log('Inserted data:', insertedData);
-			})
-			.catch(error => {
-				console.error('Error:', error);
-			});
-
-			let competence = 
-			{
-				//TODO 
-			};
-			
-			insertCompetence(competence)
-			.then(insertedData => {
-				console.log('Inserted data:', insertedData);
-			})
-			.catch(error => {
-				console.error('Error:', error);
-			});
-
 			let moyenne = 
 			{
-				//TODO 
+				noteVal: ,
+				etdId  : ,
+				modId  : ,
 			};
 			
 			insertMoyenne(moyenne)
@@ -89,4 +118,6 @@ fileMoy.addEventListener('change', () =>
 
 		}
 	}
+
+	reader.readAsArrayBuffer(file);
 });
