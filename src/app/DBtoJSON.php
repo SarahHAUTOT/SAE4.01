23c1bff20e12f3c29b0769dc2a876a48dca8afca
Ottirate <sarah.hautot76@gmail.com>
2024-03-28 13:39:53 +0100

    DB to JSON corrected (key admi missing => tab)
    + Directory for data

--- src/app/DBtoJSON.php
+++ src/app/DBtoJSON.php
@@ -1,51 +1,32 @@
 <?php
 
 // Connexion à la base de données
-$dsn = 'pgsql:host=127.0.0.1;port=5432;dbname=hs220880';
-$username = 'hs220880';
-$password = 'SAHAU2004';
+require 'DB.inc.php';
 
-try {
-    $pdo = new PDO($dsn, $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erreur de connexion à la base de données: " . $e->getMessage());
-}
-
-
-
-
-
-
-
-
-
-
+generateUsers();
+generateCompMod();
+generateYears();
 
 
 /**********************************************************************/
 /*                              USERS                                 */
 /**********************************************************************/
 
-
-// Récupérer les utilisateur 
-$query     = "SELECT * FROM Utilisateur";
-$statement = $pdo->query($query);
-$users     = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-
-// Générer le JSON
-$jsonData = json_encode($users, JSON_PRETTY_PRINT);
-// Écrire le JSON dans un fichier
-file_put_contents( 'users.json', $jsonData);
+function generateUsers()
+{
+    $db = DB::getInstance("hs220880", "hs220880", "SAHAU2004");
+    
+    // Récupérer les utilisateur 
+    $query     = "SELECT * FROM Utilisateur";
+    $users     = $db->execQuery($query);
 
 
-
-
-
-
-
-
+    // Générer le JSON
+    $jsonData = json_encode($users, JSON_PRETTY_PRINT);
+    // Écrire le JSON dans un fichier
+    file_put_contents( '../../data/users.json', $jsonData);
+    echo "Le fichier users.json a été créé avec succès.<br>";
+}
 
 
 
@@ -55,43 +36,36 @@ file_put_contents( 'users.json', $jsonData);
 /*                       COMPETENCES-MODULES                          */
 /**********************************************************************/
 
-
-// Récupérer les utilisateur 
-$query       = "SELECT * FROM Competence ORDER BY compId";
-$statement   = $pdo->query($query);
-$competences = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($competences as &$competence)
+function generateCompMod()
 {
-    $competence['modules'] = [];
+    $db = DB::getInstance("hs220880", "hs220880", "SAHAU2004");
 
-    $query       = "SELECT * FROM CompMod c JOIN Module m ON c.modId = m.modId WHERE compId =" . $competence['compid'];
-    $statement   = $pdo->query($query);
-    $modules     = $statement->fetchAll(PDO::FETCH_ASSOC);
+    // Récupérer les utilisateur 
+    $query       = "SELECT * FROM Competence ORDER BY compId";
+    $competences = $db->execQuery($query);
 
-    foreach ($modules as $module) {
-        $competence['modules'][] = [
-            'modId'   => $module['modid'],
-            'modLib'  => $module['modlib'],
-            'coef'    => $module['modcoef']
-        ];
+    foreach ($competences as &$competence)
+    {
+        $competence['modules'] = [];
+
+        $query       = "SELECT * FROM CompMod c JOIN Module m ON c.modId = m.modId WHERE compId =" . $competence['compid'];
+        $modules     = $db->execQuery($query);
+
+        foreach ($modules as $module) {
+            $competence['modules'][] = [
+                'modId'   => $module['modid'],
+                'modLib'  => $module['modlib'],
+                'coef'    => $module['modcoef']
+            ];
+        }
     }
 
-    
+    // Générer le JSON
+    $jsonData = json_encode($competences, JSON_PRETTY_PRINT);
+    // Écrire le JSON dans un fichier
+    file_put_contents( '../../data/comp.json', $jsonData);
+    echo "Le fichier comp.json a été créé avec succès.<br>";
 }
-
-
-// Générer le JSON
-$jsonData = json_encode($competences, JSON_PRETTY_PRINT);
-// Écrire le JSON dans un fichier
-file_put_contents( 'comp.json', $jsonData);
-
-
-
-
-
-
-
 
 
 
@@ -101,93 +75,88 @@ file_put_contents( 'comp.json', $jsonData);
 /*                            STUDENTS                                */
 /**********************************************************************/
 
+function generateYears()
+{
+    
+    $db = DB::getInstance("hs220880", "hs220880", "SAHAU2004");
+
+    // Récupérer les données de la table Annee
+    $query     = "SELECT * FROM Annee";
+    $years     = $db->execQuery($query);
+
+    $query     = "SELECT * FROM Semestre";
+    $semesters = $db->execQuery($query);
 
 
-// Récupérer les données de la table Annee
-$query     = "SELECT * FROM Annee";
-$statement = $pdo->query($query);
-$years     = $statement->fetchAll(PDO::FETCH_ASSOC);
+    //For each year 
+    foreach ($years as &$year) {
 
-$query     = "SELECT * FROM Semestre";
-$statement = $pdo->query($query);
-$semesters = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $year["semesters"] = [];
 
-
-//For each year 
-foreach ($years as &$year) {
-
-    $year["semesters"] = [];
-
-    foreach ($semesters as &$semester) 
-    {
-        // We get the students from this year and semester
-        $query = "SELECT * FROM Etudiant WHERE etdId IN (SELECT etdId FROM AdmComp a JOIN Competence c ON a.compId = c.compId WHERE a.anneeId = ".$year["anneeid"] . " AND semId = ".$semester["semid"] . ")";
-        $statement = $pdo->query($query);
-        $students = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-        $semesterStudents = []; // Array to hold students for this semester
-
-        // For each student
-        foreach ($students as &$student) 
+        foreach ($semesters as &$semester) 
         {
-            /* MODULES */
-            // We get the grades from this year, semester, and student
-            $query = "SELECT * FROM Moyenne m  JOIN Module mo ON mo.modId = m.modId WHERE m.anneeId = " . $year['anneeid'] . " AND etdId = " . $student["etdid"] . " AND m.modId IN (SELECT modId FROM CompMod a JOIN Competence c ON a.compId = c.compId WHERE semId = ".$semester["semid"] . ")";
-            $statement = $pdo->query($query);
-            $grades = $statement->fetchAll(PDO::FETCH_ASSOC);
+            // We get the students from this year and semester
+            $query = "SELECT * FROM Etudiant WHERE etdId IN (SELECT etdId FROM AdmComp a JOIN Competence c ON a.compId = c.compId WHERE a.anneeId = ".$year["anneeid"] . " AND semId = ".$semester["semid"] . ")";
+            $students = $db->execQuery($query);
 
-            // Put them in student
-            $student['modules'] = [];
-            foreach ($grades as $grade) {
-                $student['modules'][] = [
-                    'modId'   => $grade['modid'],
-                    'modLib'  => $grade['modlib'],
-                    'noteVal' => $grade['noteval']
-                ];
+
+            $semesterStudents = []; // Array to hold students for this semester
+
+            // For each student
+            foreach ($students as &$student) 
+            {
+                /* MODULES */
+                // We get the grades from this year, semester, and student
+                $query = "SELECT * FROM Moyenne m  JOIN Module mo ON mo.modId = m.modId WHERE m.anneeId = " . $year['anneeid'] . " AND etdId = " . $student["etdid"] . " AND m.modId IN (SELECT modId FROM CompMod a JOIN Competence c ON a.compId = c.compId WHERE semId = ".$semester["semid"] . ")";
+                $grades = $db->execQuery($query);
+
+                // Put them in student
+                $student['modules'] = [];
+                foreach ($grades as $grade) {
+                    $student['modules'][] = [
+                        'modId'   => $grade['modid'],
+                        'modLib'  => $grade['modlib'],
+                        'noteVal' => $grade['noteval']
+                    ];
+                }
+
+                /* COMPETENCES */
+                // We get the competences from this year, semester, and student
+                $query = "SELECT * FROM AdmComp a JOIN Competence c ON a.compId = a.compId WHERE anneeId = " . $year['anneeid'] . " AND semId = ".$semester["semid"]. " AND etdId = ".$student["etdid"];
+                $competences = $db->execQuery($query);
+
+                // Put them in student
+                $student['competences'] = [];
+                foreach ($competences as $competence) {
+                    $student['competences'][] = [
+                        'compId'   => $competence['compid'],
+                        'compLib'  => $competence['complib'],
+                        'admi'     => $competence['admi']
+                    ];
+                }
+
+                /* YEAR */
+                // We get the admission by year
+                $query = "SELECT * FROM AdmAnnee WHERE anneeId = " . $year['anneeid'] . " AND etdId = ".$student["etdid"];
+                $admYear = $db->execQuery($query); // Assuming only one row is returned
+
+                // Put it in student
+                $student['admi'] = $admYear[0]['admi'];
+
+                $semesterStudents[] = $student; // Add the student to the array of students for this semester
             }
 
-            /* COMPETENCES */
-            // We get the competences from this year, semester, and student
-            $query = "SELECT * FROM AdmComp a JOIN Competence c ON a.compId = a.compId WHERE anneeId = " . $year['anneeid'] . " AND semId = ".$semester["semid"]. " AND etdId = ".$student["etdid"];
-            $statement = $pdo->query($query);
-            $competences = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-            // Put them in student
-            $student['competences'] = [];
-            foreach ($competences as $competence) {
-                $student['competences'][] = [
-                    'compId'   => $competence['compid'],
-                    'compLib'  => $competence['complib'],
-                    'admi'     => $competence['admi']
-                ];
-            }
-
-            /* YEAR */
-            // We get the admission by year
-            $query = "SELECT * FROM AdmAnnee WHERE anneeId = " . $year['anneeid'] . " AND etdId = ".$student["etdid"];
-            $statement = $pdo->query($query);
-            $admYear = $statement->fetch(PDO::FETCH_ASSOC); // Assuming only one row is returned
-
-            // Put it in student
-            $student['admission'] = [
-                    'admi'     => $admYear['admi']
-                ];
-
-            $semesterStudents[] = $student; // Add the student to the array of students for this semester
+            $semester['etd'] = $semesterStudents; // Assign the array of students to the semester directly
+            $year['semesters'][] = $semester; // Add the semester to the array of semesters for this year
         }
-
-        $semester['etd'] = $semesterStudents; // Assign the array of students to the semester directly
-        $year['semesters'][] = $semester; // Add the semester to the array of semesters for this year
     }
+
+
+    // Générer le JSON
+    $jsonData = json_encode($years, JSON_PRETTY_PRINT);
+    // Écrire le JSON dans un fichier
+    file_put_contents( '../../data/donnees.json', $jsonData);
+
+    echo "Le fichier donnees.json a été créé avec succès.<br>";
 }
-
-
-
-// Générer le JSON
-$jsonData = json_encode($years, JSON_PRETTY_PRINT);
-// Écrire le JSON dans un fichier
-file_put_contents( 'donnees.json', $jsonData);
-
-echo "Les fichiers JSON a été créé avec succès.";
-
 ?>
