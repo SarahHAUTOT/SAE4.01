0b8cee2b34b403e5259c874c2608a20a7930e7aa
ShantiLKael <98842719+ShantiLKael@users.noreply.github.com>
2024-04-02 14:16:17 +0200

    getRankSem and insert sql

--- src/database/insert.sql
+++ src/database/insert.sql
@@ -24,84 +24,140 @@ ALTER SEQUENCE Annee_anneeId_seq RESTART WITH 1;
 UPDATE Annee SET anneeId = nextval('Annee_anneeId_seq');
 
 
-INSERT INTO Module (modCat, modLib)
-VALUES      ('e','R1.01'),
-            ('e','R2.02');
+INSERT INTO Competence (compCode, compLib, semId) 
+VALUES 
+	('C1 Sem1','C11', 1),
+	('C2 Sem1','C12', 1),
+	('C3 Sem1','C13', 1),
+
+	('C4 Sem2','C24', 2), -- 4
+	('C5 Sem2','C25', 2),
+	('C3 Sem2','C23', 2),
+	('C2 Sem2','C22', 2),
+
+	('C1 Sem3','C31', 3), -- 8
+	('C2 Sem3','C32', 3),
+	('C3 Sem3','C33', 3),
+
+	('C5 Sem4','C25', 4); -- 11
+	
+
+INSERT INTO Module (modId, modCat, modLib)
+VALUES      ('R11','e','R1.01'),
+			('R22','e','R2.02'),
+
+			('R21','e','R2.01'),
+			('R23','e','R2.03'),
+			('R24','e','R2.04'),
+			('R25','e','R2.05'),
+
+			('R11','e','R1.02'),
+			('R13','e','R1.03'),
+			('R14','e','R1.04'),
+
+			('R31','e','R3.01'),
+			('R32','e','R3.02'),
+			('R33','e','R3.03')
+			
+			('R42','e','R4.02');
 
 INSERT INTO Annee (anneLib) VALUES 
-    ('2021-2022'), -- Année scolaire 2021-2022
-    ('2022-2023'); -- Année scolaire 2022-2023
+	('2021-2022'), -- Année scolaire 2021-2022
+	('2022-2023'); -- Année scolaire 2022-2023
 
 
 
 INSERT INTO Semestre DEFAULT VALUES;
 INSERT INTO Semestre DEFAULT VALUES;
-
+INSERT INTO Semestre DEFAULT VALUES;
+INSERT INTO Semestre DEFAULT VALUES;
 
 INSERT INTO Etudiant (etdId, etdCiv, etdNom, etdPrenom, etdGroupeTP, etdGroupeTD, etdCursus, etdBonus, etdBac) 
 VALUES 
-    (1, 'M.', 'Dupont', 'Jean', 'Groupe1', 'TD1', 'Mathématiques', 0, 'Bac S'),
-    (2, 'Mme', 'Durand', 'Marie', 'Groupe2', 'TD2', 'Biologie', 0.5, 'Bac ES'),
-    (3, 'M.', 'Lefebvre', 'Pierre', 'Groupe3', 'TD3', 'Informatique', 1.2, 'Bac STI2D');
-
+	(1, 'M.',	 'Dupont', 	 'Jean',	 	'B', 'B1', 'S1 S2', 0, 'Bac S'),
+	(2, 'Mme',	 'Durand', 	 'Marie',	 	'C', 'C1', 'S1 S2', 0.5, 'Bac ES'),
+	(3, 'M.',	 'Lefebvre', 'Pierre', 		'C', 'C1', 'S1 S2', 1.2, 'Bac STI2D');
+	(4, 'Mme',	 'Durand',   'Trinity',     'A', 'A2', 'S1 S2', 0.5, 'Bac ES'),
+	(5, 'Mme',	 'Reau',     'Rodolf',      'A', 'A1', 'S1 S2', 0.5, 'Bac HES'),
+	(6, 'Mme',	 'Tuturu',   'Titouan',     'C', 'C2', 'S1 S2', 0.5, 'Bac HES'),
+	(7, 'Mme',	 'Yvelines', 'Jean-Michel', 'B', 'B2', 'S1 S2', 0.5, 'Bac HES'),
+	(8, 'Mme',	 'Apa',      'Gnan',        'G', 'G1', 'S1 S2 S3 S4', 0.5, 'Bac HES'),
+	(9, 'Mme',	 'Roulette', 'Robert',      'G', 'G1', 'S1 S2 S3 S4', 0.5, 'Bac HES'),
+	(10, 'Mme',	 'Random',   'Marie',       'G', 'G2', 'S1 S2 S3 S4', 0.5, 'Bac HES'),
+	(11, 'M.',	 'Lefebvre', 'Pierre',      'F', 'F2', 'S1 S2 S3 S4', 1.2, 'Bac STI2D');
 
 INSERT INTO Utilisateur (userLogin, userPassword, userDroit) 
 VALUES 
-    ('john_doe', 'password123', 1),
-    ('jane_smith', 'secret789', 2),
-    ('admin_user', 'adminpass', 1);
-
-
-INSERT INTO Competence (compCode, compLib, semId) 
-VALUES 
-    ('C.A','C.A', 1),
-    ('C.B','C.B', 1),
-    ('C.C','C.C', 1),
-    ('C.D','C.D', 2),
-    ('C.E','C.E', 2),
-    ('C.F','C.F', 2);
-
+	('john_doe', 'password123', 1),
+	('jane_smith', 'secret789', 2),
+	('admin_user', 'adminpass', 1);
 
 INSERT INTO Moyenne (noteVal, etdId, modId, anneeId) 
-VALUES (14.5, 1, 1, 1),
-       (16.8, 1, 2, 1),
-       (17.2, 2, 1, 1),
-       (15.9, 2, 2, 2),
-       (18.3, 3, 1, 2),
-       (19.1, 3, 2, 2);
+VALUES
+	(14.5, 1, 'R11', 1),
+	(16.8, 1, 'R22', 1),
+
+	(17.2, 2, 'R11', 1),
+	(17.2, 2, 'R11', 1),
+	(15.9, 2, 'R22', 2),
+
+	(18.3, 3, 'R11', 2),
+	(18.3, 3, 'R11', 2),
+	(19.1, 3, 'R22', 2);
 
 
 INSERT INTO AdmComp (etdId, compId, anneeId, admi)
 VALUES 
-    (1, 1, 1, 'ADM'),   -- Jean Dupont est admis à la compétence C.A pour l'année 1
-    (2, 1, 1, 'CMP'),   -- Marie Durand est complétée à la compétence C.A pour l'année 1
-    (3, 2, 1, 'AJ'),    -- Pierre Lefebvre est ajourné à la compétence C.B pour l'année 1
-    (1, 4, 2, 'ADM'),   -- Jean Dupont est admis à la compétence C.D pour l'année 2
-    (2, 5, 2, 'ADSUP'); -- Marie Durand est admis avec mention à la compétence C.E pour l'année 2
+	(1, 1, 1, 'ADM'),   -- Jean Dupont est admis à la compétence C.A pour l'année 1
+	(2, 1, 1, 'CMP'),   -- Marie Durand est complétée à la compétence C.A pour l'année 1
+	(3, 2, 1, 'AJ'),    -- Pierre Lefebvre est ajourné à la compétence C.B pour l'année 1
+	(1, 4, 2, 'ADM'),   -- Jean Dupont est admis à la compétence C.D pour l'année 2
+	(2, 5, 2, 'ADSUP'); -- Marie Durand est admis avec mention à la compétence C.E pour l'année 2
 
 -- Remplir la table AdmAnnee
 INSERT INTO AdmAnnee (etdId, anneeId, admi)
 VALUES 
-    (1, 1, 'ADM'),   -- Jean Dupont est admis pour l'année 1
-    (2, 1, 'PASD'),  -- Marie Durand est passé pour l'année 1
-    (3, 1, 'RED'),   -- Pierre Lefebvre est redoublant pour l'année 1
-    (1, 2, 'ADM'),   -- Jean Dupont est admis pour l'année 2
-    (2, 2, 'NAR'),   -- Marie Durand est non admis pour l'année 2
-    (3, 2, 'NR');   -- Marie Durand est non admis pour l'année 2
+	(1, 1, 'ADM'),   -- Jean Dupont est admis pour l'année 1
+	(2, 1, 'PASD'),  -- Marie Durand est passé pour l'année 1
+	(3, 1, 'RED'),   -- Pierre Lefebvre est redoublant pour l'année 1
+	(1, 2, 'ADM'),   -- Jean Dupont est admis pour l'année 2
+	(2, 2, 'NAR'),   -- Marie Durand est non admis pour l'année 2
+	(3, 2, 'NR');   -- Marie Durand est non admis pour l'année 2
 
 
 
 -- Insertion des données dans la table CompMod
 INSERT INTO CompMod (compId, modId, modCoef) VALUES
-    (1, 1, 1.5),  -- Compétence C.A pour le module R1.01 avec un coefficient de 1.5
-    (1, 2, 1.2),  -- Compétence C.A pour le module R2.02 avec un coefficient de 1.2
-    (2, 1, 1.0),  -- Compétence C.B pour le module R1.01 avec un coefficient de 1.0
-    (2, 2, 1.3),  -- Compétence C.B pour le module R2.02 avec un coefficient de 1.3
-    (3, 1, 0.8),  -- Compétence C.C pour le module R1.01 avec un coefficient de 0.8
-    (3, 2, 1.0),  -- Compétence C.C pour le module R2.02 avec un coefficient de 1.0
-    (4, 1, 1.2),  -- Compétence C.D pour le module R1.01 avec un coefficient de 1.2
-    (4, 2, 1.5),  -- Compétence C.D pour le module R2.02 avec un coefficient de 1.5
-    (5, 1, 1.0),  -- Compétence C.E pour le module R1.01 avec un coefficient de 1.0
-    (5, 2, 1.1),  -- Compétence C.E pour le module R2.02 avec un coefficient de 1.1
-    (6, 1, 0.9),  -- Compétence C.F pour le module R1.01 avec un coefficient de 0.9
-    (6, 2, 0.7);  -- Compétence C.F pour le module R2.02 avec un coefficient de 0.7
+	(1, 'R11', 1.5), -- sem 1
+	(1, 'R13', 1.2),
+
+	(2, 'R13', 1.0), 
+	(2, 'R14', 1.3),
+
+	(3, 'R13', 0.8), 
+	(3, 'R14', 1.0),
+
+	(4, 'R22', 1.2), -- sem 2
+	(4, 'R24', 1.5),
+
+	(5, 'R25', 1.0), 
+	(5, 'R24', 1.1),
+
+	(6, 'R22', 0.9), 
+	(6, 'R24', 0.7),
+
+	(7, 'R23', 1.7),
+	(7, 'R22', 0.7),
+	(7, 'R24', 1.5),
+
+	(8, 'R32', 1.0), -- sem 3
+	(8, 'R31', 0.7),
+
+	(9, 'R32', 1.2),
+	(9, 'R33', 0.7),
+
+	(10, 'R33', 1.5),
+	(10, 'R31', 0.7),
+
+	(11, 'R42', 1.0); -- sem 4
+	
