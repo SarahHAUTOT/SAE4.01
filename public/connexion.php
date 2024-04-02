8b578bf8aac5f57d53640df63fe06ee4c3e6dc92
TomGoureau <tom.goureau@gmail.com>
2024-04-02 10:52:34 +0200

    connexion

---
+++ public/connexion.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+
+$jsonData = file_get_contents('../data/users.json');
+
+$data = json_decode($jsonData, true);
+
+foreach ($variable as $key => $value) {
+	# code...
+}
+
+// Vérifier si les champs du formulaire sont soumis
+if (isset($_POST['user']) && isset($_POST['password'])) {
+	$username = $_POST['user'];
+	$password = $_POST['password'];
+
+	foreach ($data as $user) {
+		if ($user['userlogin'] === $username && $user['userpassword'] === $password) {
+			$_SESSION['username'] = $username;
+			header("Location: accueilAdmin.php");
+			exit;
+		} else {
+			// Identifiants invalides, afficher un message d'erreur
+			$errorMessage = "Nom d'utilisateur ou mot de passe incorrect.";
+		}
+	}
+}
+
+
+
+include "background.php";
+head("css/accueilEtConnexion.css"); ?>
+<h1>Connexion</h1>
+<div class="container">
+	<div class="gridRessource">
+		<span>Utilisateur :</span>
+		<input id="userName" type="text" name="user" value="">
+		<span>Mot de passe :</span>
+		<input id="password" type="password" name="password" value="">
+
+	</div>
+	<button id="valider" class="validateButtonStyle">Valider</button>
+</div>
+<?php foot("accueilEtConnexion"); ?>
